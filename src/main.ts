843d765d6ecd94b220a57c4945e5936799e05c8b
rxliuli <rxliuli@gmail.com>
2022-06-29 16:48:00 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
+import { Button } from 'ant-design-vue'
+
+console.log(Button)
+createApp(App).mount('#app')
